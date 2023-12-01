1b2d76fa4f290db2f386cf68c458951ce1ebdb1b
kabanhub <danilkarban5@gmail.com>
2023-12-01 14:22:09 +0200

    Написано тести для методів класу Calculator

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,35 +1,31 @@
-import pytest
 from app.calculator import Calculator
 
-class TestCalculator(pytest.TestCase):
-    def test_addition(self):
-        calc = Calculator(3, 4)
-        result = calc.addition()
-        self.assertEqual(result, 7)
+def test_addition_positive():
+    calc = Calculator(3, 4)
+    result = calc.addition()
+    assert result == 7
 
-    def test_subtraction(self):
-        calc = Calculator(5, 2)
-        result = calc.subtraction()
-        self.assertEqual(result, 3)
+def test_addition_negative():
+    calc = Calculator(-3, 4)
+    result = calc.addition()
+    assert result == 1
 
-    def test_multiplication(self):
-        calc = Calculator(2, 6)
-        result = calc.multiplication()
-        self.assertEqual(result, 12)
+def test_addition_float():
+    calc = Calculator(2.5, 3.5)
+    result = calc.addition()
+    assert result == 6.0
 
-    def test_division(self):
-        calc = Calculator(8, 4)
-        result = calc.division()
-        self.assertEqual(result, 2)
+def test_subtraction_positive():
+    calc = Calculator(5, 2)
+    result = calc.subtraction()
+    assert result == 3
 
-    def test_exponentiation(self):
-        calc = Calculator(2, 3)
-        result = calc.exponentiation()
-        self.assertEqual(result, 8)
+def test_subtraction_negative():
+    calc = Calculator(2, 5)
+    result = calc.subtraction()
+    assert result == -3
 
-    def test_random_number(self):
-        random_num = Calculator.random_number(1, 10)
-        self.assertTrue(1 <= random_num <= 10)
-
-if __name__ == '__main__':
-    pytest.main()
+def test_subtraction_float():
+    calc = Calculator(8.5, 3.5)
+    result = calc.subtraction()
+    assert result == 5.0
