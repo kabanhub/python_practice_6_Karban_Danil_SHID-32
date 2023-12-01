0cee18600b1299b65f2eec610e52b1d5166a0b9e
kabanhub <danilkarban5@gmail.com>
2023-12-01 15:05:16 +0200

    Написано групи тестів (позитивні та негативні) для методів класу Calculator

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,31 +1,66 @@
+import pytest
+
 from app.calculator import Calculator
 
-def test_addition_positive():
+''' Позитивні тести '''
+def test_positive_addition():
     calc = Calculator(3, 4)
     result = calc.addition()
     assert result == 7
 
-def test_addition_negative():
-    calc = Calculator(-3, 4)
-    result = calc.addition()
-    assert result == 1
-
-def test_addition_float():
-    calc = Calculator(2.5, 3.5)
-    result = calc.addition()
-    assert result == 6.0
-
-def test_subtraction_positive():
+def test_positive_subtraction():
     calc = Calculator(5, 2)
     result = calc.subtraction()
     assert result == 3
 
-def test_subtraction_negative():
+def test_positive_multiplication():
+    calc = Calculator(2, 3)
+    result = calc.multiplication()
+    assert result == 6
+
+def test_positive_multiplication_float():
+    calc = Calculator(2.5, 2)
+    result = calc.multiplication()
+    assert result == 5.0
+
+def test_positive_division():
+    calc = Calculator(8, 4)
+    result = calc.division()
+    assert result == 2
+
+def test_positive_exponentiation():
+    calc = Calculator(2, 3)
+    result = calc.exponentiation()
+    assert result == 8
+
+''' Негативні тести '''
+def test_negative_addition():
+    calc = Calculator(-3, 4)
+    result = calc.addition()
+    assert result == 1
+
+def test_negative_subtraction():
     calc = Calculator(2, 5)
     result = calc.subtraction()
     assert result == -3
 
-def test_subtraction_float():
-    calc = Calculator(8.5, 3.5)
-    result = calc.subtraction()
-    assert result == 5.0
+def test_negative_multiplication():
+    calc = Calculator(-2, 3)
+    result = calc.multiplication()
+    assert result == -6
+
+def test_negative_multiplication_float():
+    calc = Calculator(4.5, -3)
+    result = calc.multiplication()
+    assert result == -13.5
+def test_negative_division():
+    calc = Calculator(8, 0)
+    result = calc.division()
+    assert result == "Error: Division by zero"
+
+def test_negative_exponentiation():
+    calc = Calculator(-2, 3)
+    result = calc.exponentiation()
+    assert result == -8
+
+
